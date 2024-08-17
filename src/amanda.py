2035393a5e7d90ffd50234cd38499d048099a4c4
Pedro A. Neves <PedroANeves@users.noreply.github.com>
2024-08-16 21:27:27 -0300

    fixed crash on file not found

--- src/amanda.py
+++ src/amanda.py
@@ -103,7 +103,11 @@ def _add_time_delta(start: str, delta: int) -> str:
 
 def build_lines(timestamps, videos):
     return [
-        (videos[prefix], time_start, _add_time_delta(time_start, 10))
+        (
+            videos.get(prefix, "NOT_FOUND"),
+            time_start,
+            _add_time_delta(time_start, 10),
+        )
         for prefix, time_start in timestamps
     ]
 
